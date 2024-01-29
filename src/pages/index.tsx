88b360b5e314e241f888a5597b8c34f6b3703f3d
Moon-YJ <tunjeong@gmail.com>
2024-01-29 21:11:47 +0900

    Header 관련 파일 추가 및 기본 세팅

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+import type { NextPage } from 'next';
+import Header from '@/components/Header';
+
+const Home: NextPage = () => {
+	return (
+		<div>
+			<Head>
+				<title>NETFLIX</title>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+
+			<Header />
+			<main>
+				<h1 className=''>Main Page</h1>
+			</main>
+		</div>
+	);
+};
+
+export default Home;

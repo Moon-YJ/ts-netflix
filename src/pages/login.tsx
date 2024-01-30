cb6b0ed13005f67b86a443a7dc9675fe5dfdf2cf
Moon-YJ <tunjeong@gmail.com>
2024-01-30 12:19:40 +0900

    로그인 인증처리할 input 등록 및 인증 실패시 출력될 에러메세지 설정

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -5,7 +5,6 @@ import { FunctionComponent, useState } from 'react';
 import logo from '@/public/logo.svg';
 import { MoonLoader } from 'react-spinners';
 import { SubmitHandler, useForm } from 'react-hook-form';
-import { Input } from 'postcss';
 // npm i react-spinners
 // npm i react-hook-form
 
@@ -19,13 +18,18 @@ const Login: FunctionComponent = () => {
 	const [Login, setLogin] = useState<boolean>(false);
 
 	// register: 원하는 input요소를 전개연산자로 등록해서 값을 관리
-	// handleSubmit: submit이벤트 발생시 register에 등록된 input값들의 인증처리 함수
+	// handleSubmit: submit이벤트 발생시 register에 등록된 input값들의 인증처리 핸들러함수를 콜백으로 전달받음
 	// formState: 인증실패시 커스텀 에러메시지를 등록할 수 있는 객체
 	const {
 		register,
 		handleSubmit,
 		formState: { errors }, // formState객체값에서 다시 errors 프로퍼티에 등록되어있는 에러메시지만 추출
 	} = useForm<Inputs>();
+
+	const join: SubmitHandler<Inputs> = async ({ email, password }) => {
+		console.log(email, '::email');
+		console.log(password, '::pwd');
+	};
 
 	return (
 		<>
@@ -51,11 +55,28 @@ const Login: FunctionComponent = () => {
 					className='absolute top-[50%] left-[50%] ml-[-25px] mt-[-25px] z-[2]'
 				/>
 				{/* login */}
-				<form className='relative z-[5] bg-black/70 py-10 px-8 space-y-8'>
+				{/* submit 이벤트 발생시 handleSubmit이 인증처리를 해주고, 등록된 콜백함수에 인증 결과값을 전달 */}
+				<form
+					className='relative z-[5] bg-black/70 py-10 px-8 space-y-8'
+					onSubmit={handleSubmit(join)}
+				>
 					<h1 className='text-4xl font-semibold'>Sign In</h1>
 					<div className='space-y-4'>
-						<input type='email' placeholder='Email' className='input' />
-						<input type='password' placeholder='Password' className='input' />
+						<input
+							type='email'
+							placeholder='Email'
+							className='input'
+							{...register('email', { required: true })}
+						/>
+						{/* 인증 실패시 비구조할당으로 뽑아낸 errors 객체에 전달한 property명으로 에러값 전달 */}
+						{errors.email && <span>Enter valid Email address.</span>}
+						<input
+							type='password'
+							placeholder='Password'
+							className='input'
+							{...register('password', { required: true })}
+						/>
+						{errors.password && <span>Enter valid Password.</span>}
 					</div>
 					<button className='w-full rounded bg-[red] py-3 font-semibold'>
 						Sign In

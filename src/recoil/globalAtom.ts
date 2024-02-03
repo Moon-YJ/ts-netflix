a53f8834826bf4824f09f23d2c7a90eb21acd2c1
Moon-YJ <tunjeong@gmail.com>
2024-02-03 23:35:25 +0900

    next가 프리빌드시 recoil키값을 변경하는 문제 해결 - uuid 모듈설치후 빌드할때마다 고유값 다시 붙여주도록 설정

--- src/recoil/globalAtom.ts
+++ src/recoil/globalAtom.ts
@@ -1,13 +1,14 @@
 // npm i recoil
 import { atom } from 'recoil';
 import { Movie } from '../../types';
+import { v1 } from 'uuid';
 
 export const modalState = atom<boolean>({
-	key: 'modalStateKey',
+	key: `modalStateKey/${v1()}`,
 	default: false,
 });
 
 export const movieState = atom<Movie | null>({
-	key: 'movieStateKey',
+	key: `movieStateKey/${v1()}`,
 	default: null,
 });
